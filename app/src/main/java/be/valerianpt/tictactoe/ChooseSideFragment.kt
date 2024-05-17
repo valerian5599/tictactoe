5a095d3041ccec74aba6b8d98c3aa36e1e6cf1bc
Valérian Paillet <paillet.valerian@gmail.com>
2024-05-17 23:57:39 +0200

    choose side UI

--- app/src/main/java/be/valerianpt/tictactoe/ChooseSideFragment.kt
+++ app/src/main/java/be/valerianpt/tictactoe/ChooseSideFragment.kt
@@ -1,30 +1,31 @@
 package be.valerianpt.tictactoe
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import be.valerianpt.tictactoe.databinding.FragmentChooseSideBinding
 import be.valerianpt.tictactoe.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class ChooseSideFragment : Fragment() {
+    private lateinit var binding: FragmentChooseSideBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentChooseSideBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //navigation
+        //nav
         binding.homeButton.setOnClickListener {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToChooseSideFragment())
+            findNavController().navigateUp()
         }
     }
 }
